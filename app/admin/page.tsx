f7221140565dcd7516f7a2ab901ef30fd5cc6e4e
Ahmed Hosny <ahmedhosny@Ahmeds-MacBook-Pro.local>
2024-01-11 17:10:57 -0500

    fixed it

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function Admin({ name }: { name: string }) {
-    return <div>Hello, {name}!</div>;
+interface AdminProps {
+    params: { slug: string };
+    searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default function Admin({ params, searchParams }: AdminProps) {
+    return <div>Hello, </div>;
 }
