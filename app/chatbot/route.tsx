5347af7f682d3d66d07584106d672bcbdc503ce3
Ahmed Hosny <ahmedhosny@Ahmeds-MacBook-Pro.local>
2024-01-11 20:55:51 -0500

    revuild

--- app/chatbot/route.tsx
+++ app/chatbot/route.tsx
@@ -1,16 +1,16 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 // interface TokenPayload {
 //     body: {
 //         test: string;
 //     };
 // }
-// export async function POST(req: Request) {
-//     try {
-//         const { body } = req as { body: TokenPayload };
+export async function POST(req: Request) {
+    try {
+        // const { body } = req as { body: TokenPayload };
 
-//         return NextResponse.json({ message: 'Hello, World!' });
-//     } catch (err) {
-//         return NextResponse.json({ message: 'Internal server error' });
-//     }
-// }
+        return NextResponse.json({ message: 'Hello, World!' });
+    } catch (err) {
+        return NextResponse.json({ message: 'Internal server error' });
+    }
+}
